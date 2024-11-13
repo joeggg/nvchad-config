d56d00a93dcfea68f45a8a2a9f79f27896661e57
joeggg <47352958+joeggg@users.noreply.github.com>
2024-11-13 23:42:05 +0000

    fixing config for new nvchad version

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "jsonls", "taplo", "tsserver", "eslint", "sqlls", "tailwindcss" }
+local servers = { "html", "cssls", "jsonls", "taplo", "ts_ls", "eslint", "sqlls", "tailwindcss" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
